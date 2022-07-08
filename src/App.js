93c60edc853072d89cd7a952c60131b8a53f43f7
rtharisayi <42414908+rtharisayi@users.noreply.github.com>
2022-07-07 17:19:15 -0700

    updated text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Intro from "./components/intro";
 import Release from "./components/release";
 
 function App() {
-  const { qrcode, upperimage, logoversion, cardshow, login, prodHeader, release } = useFlags();
+  const { qrcode, upperimage, logoversion, cardshow, login, prodHeader, release, UpdatedText } = useFlags();
   
   return (
     <div className="App h-screen bg-ldls grid xl:grid-cols-4 grid-cols-3 grid-rows-4 xl:grid-rows-3 bg-cover bg-no-repeat">
@@ -21,7 +21,7 @@ function App() {
       (
         <header className="App-header grid col-start-1 col-span-4 row-start-2">
         <div className="body bg-ldgray px-8 py-4 mb-5 w-full shadow-2xl">
-          <p className="text-m xl:text-4xl">The common <span className="text-ldyellow">Application Delivery</span> flow...</p>
+          <p className="text-m xl:text-4xl">The common <span className="text-ldyellow">{UpdatedText}</span> flow...</p>
         </div>
         <Intro release={release} />
       </header>
